37c0814e91dc9ac006a642466f03e21271fe788d
Davis <I813096@ngic.com>
2017-12-05 11:18:28 -0500

    inital commit

---
+++ Katas/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Tests
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        [TestMethod]
+        public void RunPogram_Prints_Numbers()
+        {
+            var program = new Fizz();
+            program.RunProgram();
+        }
+    }
+}
---
+++ Katas/FizzBuzz/Fizz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class Fizz
+    {
+        static void Main(string[] args)
+        {
+           // RunProgram();
+        }
+
+        public void RunProgram()
+        {
+            var i = 1;
+            while (i <= 100)
+            {
+                if( i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                    i++;
+                    continue;
+                }
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
